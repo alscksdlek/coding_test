c454ad5b729229c8e162c6528344ad95c307db43
alscksdlek <164968618+alscksdlek@users.noreply.github.com>
2025-09-18 10:33:47 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 92 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(int num) {
+        String even = "Even";
+        String odd = "Odd";
+
+        if(num % 2 == 0) {
+            return even;
+        } else {
+            return odd;
+        }
+    }
+}
